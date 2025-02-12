063eea6f3c1530cc576bc590a941866605cf0522
habib4445 <0124habibur@gmail.com>
2025-02-12 20:37:09 +0600

    document

---
+++ index.js
@@ -0,0 +1,10 @@
+ var firstName ="Habib";
+ var lastName=" Rahman";
+  
+ var fullName= firstName+lastName;
+ document.write("My Name is "+fullName+" Habib");
+
+
+ var num1=20;
+ var num2=30;
+ document.write("  Num1=" + num1 + " and num2=" +num2)
---
+++ library function.js
@@ -0,0 +1,3 @@
+var text ="Bangladesh";
+var len = text.length;
+document.write("NUmber of character" +len);
